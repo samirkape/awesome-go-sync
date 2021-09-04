5010e634fa0f75f5bdc8729f8883513c3df8f268
samirkape <samirkape31@gmail.com>
2021-09-04 11:03:13 +0530

    Added missing lamda handler

--- cmd/go.mod
+++ cmd/go.mod
@@ -2,8 +2,7 @@ module main
 
 require (
 	github.com/aws/aws-lambda-go v1.26.0
-	github.com/samirkape/awesome-go-sync v0.9.0
-	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
+	github.com/samirkape/awesome-go-sync v0.10.0
 )
 
 go 1.16
--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"github.com/aws/aws-lambda-go/lambda"
 	parser "github.com/samirkape/awesome-go-sync"
 )
 
 func main() {
 	// Make the handler available for Remote Procedure Call by AWS Lambda
-	parser.Sync()
+	lambda.Start(parser.Sync)
 }
